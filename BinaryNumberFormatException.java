7b76727c6824d2d441ca7320a38e0345e5b30140
Kaelan <kstrones@yahoo.com>
2016-01-28 18:35:59 -0700

    Update BinaryNumberFormatException.java

--- BinaryNumberFormatException.java
+++ BinaryNumberFormatException.java
@@ -1,4 +1,15 @@
 
-public class BinaryNumberFormanException {
+public class BinaryNumberFormatException extends Exception {
+	
+	private int value;
+	
+	public BinaryNumberFormatException(int binaryVal){
+		super("Invalid Number" + binaryVal);
+		value = binaryVal;
+	}
+	
+	public double getVal(){
+		return value;
+	}
 
 }
