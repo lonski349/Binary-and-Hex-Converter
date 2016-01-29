ea594e436fa561a847eb940177f07e97f8926e08
Kaelan <kstrones@yahoo.com>
2016-01-28 23:51:22 -0700

    Update HexNumberFormatException.java

--- HexNumberFormatException.java
+++ HexNumberFormatException.java
@@ -1,4 +1,15 @@
 
-public class HexNumberFormatException {
+public class HexNumberFormatException extends Exception{
 
+	private int value;
+	
+	public HexNumberFormatException(char hexVal){
+		super("Invalid Number: " + hexVal);
+		value = hexVal;
+	}
+	
+	public double getVal(){
+		return value;
+	}
+	
 }
