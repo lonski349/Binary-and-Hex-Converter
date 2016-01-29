779c8a2c8418a89253ac391e565557368263a896
Kaelan <kstrones@yahoo.com>
2016-01-28 23:50:40 -0700

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -21,7 +21,41 @@ public class Assignment2 {
 				 * If there is a number other than one and zero throw
 				 * a binary number format
 				 */
-				throw new BinaryNumberFormatException(tempVal);
+				throw new BinaryNumberFormatException(Input.charAt((length - 1) - i));
+			}
+		}
+		
+		return decimalVal;
+	}
+	
+	public static int HexStrtoInt (String Input) throws HexNumberFormatException{
+		int decimalVal = 0; 
+		int length = Input.length();
+		
+		for (int i = 0; i < length; i++){
+			char tempVal = Input.charAt((length - 1) - i);
+			
+			if (Character.isDigit(tempVal)){
+				int intVal = Character.getNumericValue(tempVal);
+				decimalVal += intVal * (int)Math.pow(16, i);
+			}
+			
+			if (!Character.isDigit(tempVal)){
+				switch (tempVal){
+				case 'a': decimalVal += 10 * (int)Math.pow(16, i);
+						break;
+				case 'b': decimalVal += 11 * (int)Math.pow(16, i);
+						break;
+				case 'c': decimalVal += 12 * (int)Math.pow(16, i);
+						break;
+				case 'd': decimalVal += 13 * (int)Math.pow(16, i);
+						break;
+				case 'e': decimalVal += 14 * (int)Math.pow(16, i);
+						break;
+				case 'f': decimalVal += 15 * (int)Math.pow(16, i);
+						break;
+				default: throw new HexNumberFormatException(tempVal);
+				}				
 			}
 		}
 		
@@ -41,6 +75,19 @@ public class Assignment2 {
 			finalDecimalVal = BinaryStrtoInt(UsrInput);
 		}
 		catch(BinaryNumberFormatException ex){
+			System.out.println(ex);
+		}
+		
+		System.out.println(finalDecimalVal);
+		
+		finalDecimalVal = 0;
+		System.out.println("Please input a hex number");
+		UsrInput = stdin.readLine();
+		
+		try {
+			finalDecimalVal = HexStrtoInt(UsrInput);
+		}
+		catch(HexNumberFormatException ex){
 			System.out.println(ex);
 		}
 		
